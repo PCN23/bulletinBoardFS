ffca9b013bc544b6ce22d64ff0894b4715d19344
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-18 14:03:53 -0700

    adding post it form

--- create-page/create-page.js
+++ create-page/create-page.js
@@ -1,4 +1,22 @@
-import { fetchPosts } from '../fetch-utils.js';
+import { checkAuth, logout } from '../fetch-utils.js';
+
+checkAuth();
+
+const postItForm = document.getElementById('post-it-form');
+postItForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+});
+
+const logoutButton = document.getElementById('logout');
+logoutButton.addEventListener('click', () => {
+    logout();
+});
+
+const home = document.getElementById('home');
+home.addEventListener('click', () => {
+    location.replace('/');
+});
 
 
 
