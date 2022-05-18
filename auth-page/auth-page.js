b9a66645ff9c55731983143a5769dfc583a94771
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-18 13:04:51 -0700

    added second form in suth page

--- auth-page/auth-page.js
+++ auth-page/auth-page.js
@@ -1,16 +1,21 @@
-import { redirectIfLoggedIn } from '../fetch-utils.js';
+import { redirectIfLoggedIn, signInUser, signUpUser } from '../fetch-utils.js';
 
 const signUpForm = document.getElementById('sign-up');
 const signInForm = document.getElementById('sign-in');
 
+redirectIfLoggedIn();
 
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    console.log({ email: data.get('email'), password: data.get('password') });
 
-
-
-
-
-
-
+    const user = await signUpUser(data.get('email'), data.get('password'));
+    console.log(user);
+    if (user) {
+        location.replace('./auth-page');
+    }
+});
 
 signInForm.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -24,16 +29,3 @@ signInForm.addEventListener('submit', async (e) => {
     }
 });
 
-
-
-
-
-
-
-checkAuth();
-
-const logoutBtn = document.getElementById('logout');
-
-logoutBtn.addEventListener('click', async () => {
-    await logout();
-});
